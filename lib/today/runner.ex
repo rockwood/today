0e1d6d2a225fc704537889533a95fd48d1c75a25
Kevin Rockwood <kevin@rockwood.me>
2016-06-17 11:29:45 +0900

    Update Runner to use Format filename and headings

--- lib/today/runner.ex
+++ lib/today/runner.ex
@@ -1,8 +1,27 @@
 defmodule Today.Runner do
-  def run(%{message: message, dir: dir}) do
-    File.mkdir_p(dir)
-    File.open "#{dir}/something.txt", [:utf8, :append], fn(file) ->
-      IO.write(file, message)
+  alias Today.Format
+
+  def run(message, dir \\ System.user_home) do
+    create_dir(dir)
+    File.open "#{dir}/#{Format.filename}", [:utf8, :read, :append], fn(file) ->
+      write_file(file, message)
     end
+  end
+
+  defp create_dir(dir) do
+    File.mkdir_p(dir)
+  end
+
+  defp write_file(file, message) do
+    write_heading(file)
+    IO.write(file, message)
+  end
+
+  defp write_heading(file) do
+    if IO.gets(file, :all) == :eof do
+      IO.write(file, Format.date_heading)
+    end
+
+    IO.write(file, Format.time_heading)
   end
 end
--- test/today/runner_test.exs
+++ test/today/runner_test.exs
@@ -1,17 +1,34 @@
 defmodule Today.RunnerTest do
   use ExUnit.Case
-  alias Today.Runner
+  alias Today.{Format, Runner}
   @test_dir "tmp/test"
 
-  setup do
+  describe "Runner.run/1: for the first time" do
+    setup [:create_test_dir, :write_message]
+
+    test "creates today's file" do
+      file_contents = File.read!("#{@test_dir}/#{Format.filename}")
+      assert file_contents =~ "Hello World"
+    end
+  end
+
+  describe "Runner.run/1: multiple times" do
+    setup [:create_test_dir, :write_message, :write_message]
+
+    test "appends to today's file" do
+      file_contents = File.read!("#{@test_dir}/#{Format.filename}")
+      assert file_contents =~ "Hello World"
+    end
+  end
+
+  defp create_test_dir(_) do
     File.mkdir_p(@test_dir)
     on_exit fn ->
       File.rm_rf(@test_dir)
     end
   end
 
-  test "creating a file" do
-    Runner.run(%{dir: @test_dir, message: "Hello World"})
-    assert File.read!("#{@test_dir}/something.txt") == "Hello World"
+  defp write_message(_) do
+    Runner.run("Hello World", @test_dir)
   end
 end
